4399c27758305a50d061070ecf5acb63e17ca8eb
Aneesh Chodisetty <ch.aneesh1996@gmail.com>
2022-12-15 00:11:15 -0500

    [task no: 2.10]Modified Feq and amplitude

--- kamikaze/src/spawn_controller.cpp
+++ kamikaze/src/spawn_controller.cpp
@@ -15,17 +15,17 @@ SpawnController::SpawnController(const std::string &node_name)
     // publisher_ = this->create_publisher<geometry_msgs::msg::Twist>(topic_name, 10);
 
     // Starting the publisher.
-  timer_publisher_ = this->create_wall_timer(std::chrono::milliseconds(1000), std::bind(&SpawnController::timer_callback, this));
+  timer_publisher_ = this->create_wall_timer(std::chrono::milliseconds(100), std::bind(&SpawnController::timer_callback, this));
   callback_counter_ = 0;
 }
 
 void SpawnController::timer_callback() {
     // Logging the message in the terminal.
   RCLCPP_INFO(this->get_logger(), "Controlling the bots.");
-  bot_velocity_.linear.x = -0.4;
+  bot_velocity_.linear.x = -0.1;
   
   for (int i = 0; i < 20; i++) {
-    if (i <= callback_counter_) {
+    if ((i * 5)  <= callback_counter_) {
       bot_velocity_.angular.z = 0.2;
     }
     else
