7618625e60c34712af2b27957a12311c307c09c8
roboticistjoseph <jkatak73@terpmail.umd.edu>
2022-12-15 14:59:04 -0500

    [Task ID: 2.10] Updating executable

--- kamikaze/src/spawn_controller.cpp
+++ kamikaze/src/spawn_controller.cpp
@@ -27,18 +27,15 @@
 #include <spawn_controller.h>
 
 SpawnController::SpawnController(const std::string& node_name)
-  : Node(node_name)
-{
+  : Node(node_name) {
   std::string topic_name;
 
   // int iterator{0};
-  for (int i = 0; i < 20; i++)
-  {
+  for (int i = 0; i < 20; i++) {
     topic_name = "/box_bot" + std::to_string(i) + "/cmd_vel";
     std::cout << topic_name;
-    publishers_[i] = this->create_publisher<geometry_msgs::msg::Twist>(topic_name,
-                                                                       10);
-
+    publishers_[i] = this->create_publisher
+                  <geometry_msgs::msg::Twist>(topic_name, 10);
     // iterator++;
   }
 
@@ -53,20 +50,15 @@ SpawnController::SpawnController(const std::string& node_name)
   callback_counter_ = 0;
 }
 
-void SpawnController::timer_callback()
-{
+void SpawnController::timer_callback() {
   // Logging the message in the terminal.
   RCLCPP_INFO(this->get_logger(), "Controlling the bots.");
   bot_velocity_.linear.x = -0.1;
 
-  for (int i = 0; i < 20; i++)
-  {
-    if ((i * 5)  <= callback_counter_)
-    {
+  for (int i = 0; i < 20; i++) {
+    if ((i * 5)  <= callback_counter_) {
       bot_velocity_.angular.z = 0.2;
-    }
-    else
-    {
+    } else {
       bot_velocity_.angular.z = 0.0;
     }
     publishers_.at(i)->publish(bot_velocity_);
